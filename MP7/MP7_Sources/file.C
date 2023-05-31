05b8390d06e0b8cbcfdae1dd7a8bc7e1a2e243dc
shweta_sharma_1999 <shirochou@Shwetas-MacBook-Air.local>
2023-05-30 23:17:37 -0500

    Added files for Machine Problem 7

---
+++ MP7/MP7_Sources/file.C
@@ -0,0 +1,106 @@
+/*
+     File        : file.C
+
+     Author      : Riccardo Bettati
+     Modified    : 2021/11/28
+
+     Description : Implementation of simple File class, with support for
+                   sequential read/write operations.
+*/
+
+/*--------------------------------------------------------------------------*/
+/* DEFINES */
+/*--------------------------------------------------------------------------*/
+
+/* -- (none) -- */
+
+/*--------------------------------------------------------------------------*/
+/* INCLUDES */
+/*--------------------------------------------------------------------------*/
+
+#include "assert.H"
+#include "console.H"
+#include "file.H"
+
+/*--------------------------------------------------------------------------*/
+/* CONSTRUCTOR/DESTRUCTOR */
+/*--------------------------------------------------------------------------*/
+
+File::File(FileSystem *_fs, int _id) {
+    Console::puts("Opening file with id");Console::puti(_id);Console::puts(".\n");
+    fle_system = _fs;
+    fle_identifier = _id;
+    curr_pos = 0;
+    
+    bool fle_blk_found = false;
+    unsigned int i=0;
+    while(i<fle_system->MAX_INODES){
+        if(fle_system->inodes[i].id==fle_identifier){
+            inode_indx = i;
+            blk_number = fle_system->inodes[i].blk_number;
+            fle_size = fle_system->inodes[i].fle_size;
+            fle_blk_found = true;
+            break;
+        }
+        i++;
+    }
+    
+    assert(fle_blk_found);
+}
+
+File::~File() {
+    Console::puts("Closing file.\n");
+    /* Make sure that you write any cached data to disk. */
+    /* Also make sure that the inode in the inode list is updated. */
+
+    fle_system->disk->write(blk_number, block_cache);
+    fle_system->inodes[inode_indx].fle_size = fle_size;
+
+    Inode* tmp_inodes_ref = fle_system->inodes;
+    unsigned char* tmp_inode_ref = (unsigned char*) tmp_inodes_ref;
+    fle_system->disk->write(1,tmp_inode_ref);
+}
+
+/*--------------------------------------------------------------------------*/
+/* FILE FUNCTIONS */
+/*--------------------------------------------------------------------------*/
+
+int File::Read(unsigned int _n, char *_buf) {
+    Console::puts("reading from file\n");
+    int char_cnt = 0;
+    for(int indx = curr_pos; indx<fle_size;indx++){
+        if(char_cnt==_n)
+            break;
+        _buf[char_cnt++] = block_cache[indx];
+    }
+
+    Console::puts("reading from file complete.\n");
+    return char_cnt;
+}
+
+int File::Write(unsigned int _n, const char *_buf) {
+    Console::puts("writing to open file.\n");
+    int char_cnt = 0;
+    int end_indx = curr_pos+_n;
+    while(curr_pos<end_indx){
+        if(curr_pos==SimpleDisk::BLOCK_SIZE){
+            Console::puts("EOF reached while writing.\n");
+            break;
+        }
+        block_cache[curr_pos++]=_buf[char_cnt++];
+        fle_size++;
+    }
+
+    Console::puts("writing to file complete.\n");
+    return char_cnt;
+}
+
+void File::Reset() {
+    Console::puts("resetting file\n");
+    curr_pos=0;
+}
+
+bool File::EoF() {
+    Console::puts("checking for EoF\n");
+    return (curr_pos<fle_size);
+}
---
+++ MP7/MP7_Sources/file_system.C
@@ -0,0 +1,226 @@
+/*
+     File        : file_system.C
+
+     Author      : Riccardo Bettati
+     Modified    : 2021/11/28
+
+     Description : Implementation of simple File System class.
+                   Has support for numerical file identifiers.
+ */
+
+/*--------------------------------------------------------------------------*/
+/* DEFINES */
+/*--------------------------------------------------------------------------*/
+#define FREE_BLK_REP 'F'
+#define USED_BLK_REP 'U'
+#define INODE_BLK_NUMBER 1
+#define BITMAP_BLK_NUMBER 0
+
+/*--------------------------------------------------------------------------*/
+/* INCLUDES */
+/*--------------------------------------------------------------------------*/
+
+#include "assert.H"
+#include "console.H"
+#include "file_system.H"
+
+/*--------------------------------------------------------------------------*/
+/* CLASS Inode */
+/*--------------------------------------------------------------------------*/
+
+/* You may need to add a few functions, for example to help read and store 
+   inodes from and to disk. */
+
+Inode::Inode(){
+    fs = NULL;
+    id = -1;
+    //unsigned int blk_number;
+    inode_is_free = true;
+    fle_size = 0;
+}
+
+/*--------------------------------------------------------------------------*/
+/* CLASS FileSystem */
+/*--------------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------------*/
+/* CONSTRUCTOR */
+/*--------------------------------------------------------------------------*/
+
+FileSystem::FileSystem() {
+    Console::puts("In file system constructor.\n");
+    disk = NULL;
+    size = 0;
+    free_blk_cnt = SimpleDisk::BLOCK_SIZE / sizeof(unsigned char);
+    inode_cntr = 0;
+    
+    free_blocks = new unsigned char[free_blk_cnt];
+    for(unsigned int i=0;i<free_blk_cnt;i++){
+        free_blocks[i] = FREE_BLK_REP;
+    }
+    inodes = new Inode[MAX_INODES];
+    
+    Console::puts("FileSystem constructor initialized.\n");
+}
+
+FileSystem::~FileSystem() {
+    Console::puts("unmounting file system\n");
+    /* Make sure that the inode list and the free list are saved. */
+       disk->write(BITMAP_BLK_NUMBER, free_blocks);
+    unsigned char* tmp_inode_ref = (unsigned char*) inodes;
+    disk->read(INODE_BLK_NUMBER, tmp_inode_ref);
+
+    Console::puts("unmounted file system.\n");
+}
+
+
+/*--------------------------------------------------------------------------*/
+/* FILE SYSTEM FUNCTIONS */
+/*--------------------------------------------------------------------------*/
+
+
+bool FileSystem::Mount(SimpleDisk * _disk) {
+    Console::puts("mounting file system from disk\n");
+
+    /* Here you read the inode list and the free list into memory */
+    
+    disk = _disk;
+    unsigned char* tmp_inode_ref;
+
+    disk->read(BITMAP_BLK_NUMBER, free_blocks);
+    disk->read(INODE_BLK_NUMBER, tmp_inode_ref);
+
+    inodes = (Inode *) tmp_inode_ref;
+
+    // finding a free inode
+    inode_cntr = 0;
+    unsigned int i = 0;
+    while(i<MAX_INODES){
+        if(!inodes[i].inode_is_free){
+            inode_cntr++;
+        }
+        i++;
+    }
+
+    free_blk_cnt = SimpleDisk::BLOCK_SIZE / sizeof(unsigned char);
+
+    Console::puts("Mounted file system from disk.\n");
+
+    return true;
+}
+
+bool FileSystem::Format(SimpleDisk * _disk, unsigned int _size) { // static!
+    Console::puts("formatting disk\n");
+    /* Here you populate the disk with an initialized (probably empty) inode list
+       and a free list. Make sure that blocks used for the inodes and for the free list
+       are marked as used, otherwise they may get overwritten. */
+    unsigned int n_free_blks  = _size/SimpleDisk::BLOCK_SIZE;
+    unsigned char* free_blks_arr = new unsigned char[n_free_blks];
+    
+    // reserved blocks
+    free_blks_arr[0] = free_blks_arr[1] = USED_BLK_REP;
+    
+    //freeing blocks
+    unsigned int i = 2;
+    while(i<n_free_blks){
+        free_blks_arr[i] = FREE_BLK_REP;
+        i++;
+    }
+    
+    _disk->write(BITMAP_BLK_NUMBER, free_blks_arr);
+    
+    Inode* tmp_inodes_ref = new Inode[MAX_INODES];
+    unsigned char* tmp_inode_ref = (unsigned char*) tmp_inodes_ref;
+    _disk->write(INODE_BLK_NUMBER,tmp_inode_ref);
+    
+    Console::puts("formatted disk.\n");
+    return true;
+}
+
+Inode * FileSystem::LookupFile(int _file_id) {
+    Console::puts("looking up file with id = "); Console::puti(_file_id); Console::puts("\n");
+    /* Here you go through the inode list to find the file. */
+    unsigned int i;
+    for(i=0;i<inode_cntr;i++){
+        if(inodes[i].id== _file_id){
+            return &inodes[i];
+        }    
+    }
+    Console::puts("File with id = "); Console::puti(_file_id); Console::puts(" not found.\n");
+    return NULL;
+}
+
+bool FileSystem::CreateFile(int _file_id) {
+    Console::puts("creating file with id:"); Console::puti(_file_id); Console::puts("\n");
+    /* Here you check if the file exists already. If so, throw an error.
+       Then get yourself a free inode and initialize all the data needed for the
+       new file. After this function there will be a new file on disk. */
+    unsigned int i;
+    for(i=0;i<inode_cntr;i++){
+        if(inodes[i].id== _file_id){
+            Console::puts("Error: File with id: ");Console::puti(_file_id); Console::puts(" already exists.\n");
+            assert(false);
+        }    
+    }
+    
+    
+    unsigned int free_inode_indx = -1;
+    for(i=0;i<MAX_INODES;i++){
+        // Console::puti(inodes[i].inode_is_free);Console::puts(", ");
+        if(inodes[i].inode_is_free){
+            free_inode_indx = i;
+            // Console::puts("\n free_inode_indx: ");Console::puti(free_inode_indx);Console::puts("\n");
+            break;
+        }    
+    }
+    // Console::puts("Prininting free_blocks: ");
+    unsigned int free_blk_indx = -1;
+    for(i=0;i<free_blk_cnt;i++){
+        if(free_blocks[i] == FREE_BLK_REP){
+            // Console::puts("\n free_blocks: ");Console::putch(free_blocks[i]);Console::puts(",");
+            free_blk_indx = i;
+            break;
+        }    
+    }
+    // Console::puts("\n");
+    
+    assert((free_inode_indx!=-1) && (free_blk_indx!=-1));
+    inodes[free_inode_indx].inode_is_free = false;
+    inodes[free_inode_indx].fs = this;
+    inodes[free_inode_indx].id = _file_id;
+    inodes[free_inode_indx].blk_number = free_blk_indx;
+    free_blocks[free_inode_indx] = USED_BLK_REP;
+    
+    Console::puts("File created successfully.\n");
+    
+    return true;
+}
+
+bool FileSystem::DeleteFile(int _file_id) {
+    Console::puts("deleting file with id:"); Console::puti(_file_id); Console::puts("\n");
+    /* First, check if the file exists. If not, throw an error. 
+       Then free all blocks that belong to the file and delete/invalidate 
+       (depending on your implementation of the inode list) the inode. */
+
+    bool file_exists = false;
+    unsigned int indx=0;
+    
+    while(indx<MAX_INODES){
+        if(inodes[indx].id ==_file_id){
+            file_exists = true;
+            break; 
+        }
+        indx++;
+    }
+    
+    assert(file_exists);
+    
+    int blk_number = inodes[indx].blk_number;
+    free_blocks[blk_number] = FREE_BLK_REP;
+    
+    inodes[indx].inode_is_free = true;
+    inodes[indx].fle_size = 0;
+    
+    Console::puts("File deleted successfully.\n");
+    return true;
+}
